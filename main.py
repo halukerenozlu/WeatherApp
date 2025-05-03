6f5e57b4f8aaccd59ce830f159b90bef103e60f2
AakifY03 <105167848+AakifY03@users.noreply.github.com>
2025-05-04 00:36:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from cProfile import label
+from re import search
+from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from havadurumu import get_weather, icon_url
+
+
+def main():
+    city = cityEntry.get()
+    weather = get_weather(city)
+
+    if weather:
+        locationLabel['text'] = '{},{}'.format(weather[0], weather[1])
+        templabel['text'] = '{}°C'.format(weather[2])
+        conditionLabel['text'] =  weather[4]
+        icon = ImageTk.PhotoImage(Image.open(requests.get(icon_url.format(weather[3]), stream=True).raw))
+        iconLabel.configure(image=icon)
+        iconLabel.image = icon
+
+
+
+app = Tk()
+app.geometry('500x500')
+app.title('Hava durumu')
+
+cityEntry= Entry(app,justify=CENTER)
+cityEntry.pack(fill=BOTH, ipady = 10, padx = 18, pady=5)
+cityEntry.focus()
+
+searchButton = Button(app,text='Arama',font=('Arial',15), command=main)
+searchButton.pack(fill=BOTH, ipady = 10, padx = 20)
+
+iconLabel= Label(app)
+iconLabel.pack()
+
+locationLabel= Label(app, font=('Arial',40))
+locationLabel.pack()
+
+templabel = Label(app, font=('Arial',50,'bold'))
+templabel.pack()
+
+conditionLabel= Label(app, font=('Arial',20))
+conditionLabel.pack()
+
+app.mainloop()
