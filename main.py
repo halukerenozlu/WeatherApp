820ab02ecb7aa6f12b31a243079c615bc6c324a8
AakifY03 <105167848+AakifY03@users.noreply.github.com>
2025-05-04 18:01:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,25 @@
 from tkinter import *
-from PIL import Image, ImageTk
-import requests
-from havadurumu import get_weather, icon_url
+from havadurumu import get_weather
+from update import guncelle
+from page import panel
 
 
 def main():
     city = cityEntry.get()
     weather = get_weather(city)
-
-    if weather:
-        locationLabel['text'] = '{},{}'.format(weather[0], weather[1])
-        templabel['text'] = '{}°C'.format(weather[2])
-        conditionLabel['text'] = weather[4]
-        icon = ImageTk.PhotoImage(Image.open(requests.get(
-            icon_url.format(weather[3]), stream=True).raw))
-        iconLabel.configure(image=icon)
-        iconLabel.image = icon
+    guncelle(weather, locationLabel, templabel, conditionLabel, iconLabel)
 
 
 app = Tk()
 app.geometry('500x500')
 app.title('Hava durumu')
 
-cityEntry = Entry(app, justify=CENTER)
-cityEntry.pack(fill=BOTH, ipady=10, padx=18, pady=5)
-cityEntry.focus()
+# Paneli kur, widget'ları al
+cityEntry, locationLabel, templabel, conditionLabel, iconLabel = panel(app, main)
 
-searchButton = Button(app, text='Arama', font=('Arial', 15), command=main)
-searchButton.pack(fill=BOTH, ipady=10, padx=20)
-
-iconLabel = Label(app)
-iconLabel.pack()
-
-locationLabel = Label(app, font=('Arial', 40))
-locationLabel.pack()
-
-templabel = Label(app, font=('Arial', 50, 'bold'))
-templabel.pack()
-
-conditionLabel = Label(app, font=('Arial', 20))
-conditionLabel.pack()
 
 app.mainloop()
+
+
+
+
